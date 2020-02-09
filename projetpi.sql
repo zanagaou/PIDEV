5b388c8bd7893e462e7763819f71e7d0ccd32e90
samybej <49165524+samybej@users.noreply.github.com>
2020-02-09 15:37:15 +0100

    Add files via upload

--- projetpi.sql
+++ projetpi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3308
--- Généré le :  Dim 09 fév. 2020 à 10:41
--- Version du serveur :  5.7.28
--- Version de PHP :  7.3.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 09 fév. 2020 à 14:31
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -85,15 +85,14 @@ CREATE TABLE IF NOT EXISTS `chauffeur` (
   `id_vehicule` int(30) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_vehicule` (`id_vehicule`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `chauffeur`
 --
 
 INSERT INTO `chauffeur` (`id`, `cin`, `nom`, `prenom`, `tel`, `sexe`, `date_naissance`, `salaire`, `note`, `image`, `etat_compte`, `role`, `num_permis`, `mdp`, `id_vehicule`) VALUES
-(1, 1133, 'aziz', 'alaa', 55231, 'homme', '0011-12-11', 133.2, 4.3, NULL, 1, 'taxi', 4455, 'hahaha', 28),
-(5, 1133, 'aziz', 'alaa', 55231, 'homme', '0011-12-11', 133.2, 4.3, NULL, 1, 'taxi', 4455, 'hahaha', 28);
+(6, 1133, 'aziz', 'alaa', 55231, 'homme', '0011-12-11', 133.2, 4.3, NULL, 1, 'taxi', 4455, 'hahaha', 28);
 
 -- --------------------------------------------------------
 
@@ -113,15 +112,30 @@ CREATE TABLE IF NOT EXISTS `client` (
   `image` varbinary(6000) DEFAULT NULL,
   `mail` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
 --
 
 INSERT INTO `client` (`id`, `nom`, `prenom`, `tel`, `adresse`, `mdp`, `etat_compte`, `image`, `mail`) VALUES
-(44, 'khalil', 'bejaoui', 1231, 'menzah 1', 'hello', 0, NULL, 'hh@hh'),
-(104, 'khalil', 'bejaoui', 1231, 'menzah 1', 'hello', 0, NULL, 'hh@hh');
+(44, 'khalil', 'bejaoui', 1231, 'menzah 1', 'hello', 0, NULL, 'hh@hh');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formation`
+--
+
+DROP TABLE IF EXISTS `formation`;
+CREATE TABLE IF NOT EXISTS `formation` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `titre` varchar(40) NOT NULL,
+  `lieu` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -159,14 +173,7 @@ CREATE TABLE IF NOT EXISTS `offre` (
   PRIMARY KEY (`id`),
   KEY `id_offreur` (`id_offreur`),
   KEY `id_client` (`id_client`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `offre`
---
-
-INSERT INTO `offre` (`id`, `nb_place`, `depart`, `arrive`, `date`, `tarif`, `id_offreur`, `id_client`) VALUES
-(15, 4, 'kasserine', 'sousse', '0011-12-11', 4.2, 44, 44);
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -185,14 +192,26 @@ CREATE TABLE IF NOT EXISTS `postulation` (
   `id_client` int(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_client` (`id_client`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `postulation`
+-- Structure de la table `reclamation`
 --
 
-INSERT INTO `postulation` (`id`, `date_demande`, `num_permis`, `cin`, `sexe`, `etat`, `id_client`) VALUES
-(31, '0011-12-11', 4512, 11223, 'homme', 1, 44);
+DROP TABLE IF EXISTS `reclamation`;
+CREATE TABLE IF NOT EXISTS `reclamation` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `message` varchar(50) NOT NULL,
+  `date_rec` date NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `id_client` int(50) NOT NULL,
+  `id_chauffeur` int(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_client` (`id_client`),
+  KEY `id_chauffeur` (`id_chauffeur`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -213,7 +232,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   PRIMARY KEY (`id`),
   KEY `id_chauffeur` (`id_chauffeur`),
   KEY `id_client` (`id_client`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -230,15 +249,14 @@ CREATE TABLE IF NOT EXISTS `vehicule` (
   `type` varchar(20) NOT NULL,
   `etat` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `vehicule`
 --
 
 INSERT INTO `vehicule` (`id`, `immatriculation`, `numero_assurance`, `marque`, `type`, `etat`) VALUES
-(28, '126 tunis 2030', 1111, 'Ford', 'Taxi', 'mal'),
-(39, '126 tunis 2030', 1111, 'Ford', 'Taxi', 'mal');
+(28, '126 tunis 2030', 1111, 'Ford', 'Taxi', 'mal');
 
 --
 -- Contraintes pour les tables déchargées
@@ -268,6 +286,13 @@ ALTER TABLE `offre`
 --
 ALTER TABLE `postulation`
   ADD CONSTRAINT `FK_client_post` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `reclamation`
+--
+ALTER TABLE `reclamation`
+  ADD CONSTRAINT `FK_reclamation_chauffeur` FOREIGN KEY (`id_chauffeur`) REFERENCES `chauffeur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_reclamation_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `reservation`
