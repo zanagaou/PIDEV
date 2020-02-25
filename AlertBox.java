a453063083d5d5286f0ebbd05e075dbe457afc9a
samybej <49165524+samybej@users.noreply.github.com>
2020-02-25 17:45:37 +0100

    Add files via upload

---
+++ AlertBox.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import Entity.Client;
+import Entity.Offre;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Asus
+ */
+public class AlertBox {
+    
+     public static Button closeButton = new Button("OK");
+     public static Button annulerButton = new Button("annuler");
+      
+    public static void display(String title,String message){
+        Stage window =new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+        
+        Label label = new Label();
+        label.setText(message);
+       
+        closeButton.setOnAction(e -> window.close());
+        annulerButton.setOnAction(e -> window.close() );
+        
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton, annulerButton);
+        layout.setAlignment(Pos.CENTER);
+        
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+    
+    
+}
